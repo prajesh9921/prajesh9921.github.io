345b2c3db1b76c9fc03e1190feb283763f4d2d02
prajesh9921 <69838722+prajesh9921@users.noreply.github.com>
2023-01-05 00:13:19 +0530

    Add files via upload
    files of site uploaded

---
+++ index.js
@@ -0,0 +1,66 @@
+
+let TechnologiesUsed = tns({
+    container: ".my-slider",
+    "slideBy": "2",
+    "speed": 1000,
+    "nav": false,
+    autoplay: true,
+    controls: false,
+    autoplayButtonOutput: false,
+    responsive: {
+        1600: {
+            items: 7,
+            gutter: 20
+        },
+        1024: {
+            items: 6,
+            gutter: 20
+        },
+        768: {
+            items: 3,
+            gutter: 20
+        },
+        480: {
+            items: 1,
+            gutter: 20
+        },
+    }
+})
+
+function SmartLed()
+{
+    window.open(
+        'https://youtu.be/phHhytCdNdI',
+        '_blank'
+      );
+} 
+
+function FoodApp(){
+    window.open(
+        'https://github.com/prajesh9921/Food-App-',
+        '_blank'
+    )
+}
+
+function Bmi(){
+    window.open(
+        'https://github.com/prajesh9921/BMI-Calculator/tree/BMI_calculator',
+        '_blank'
+    )
+}
+
+function FigmaApp(){
+    window.open(
+        'https://www.figma.com/file/lbLStU9aiRKCii9pFf14Ak/Untitled?t=sHwgmqV0ViwByK8s-1',
+        '_blank'
+    )
+}
+
+function NotesApp(){
+    window.open(
+        'https://github.com/prajesh9921/Notes-App---React-Native',
+        '_blank'
+    )
+}
+
+
